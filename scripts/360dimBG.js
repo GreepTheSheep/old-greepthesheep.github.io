1ffb7606dc9814f8292b4bbe728843e791de72e1
Matthieu Dehon <42576124+GreepTheSheep@users.noreply.github.com>
2021-11-23 18:15:16 +0100

    Added dim foreground on no mouse mouve

---
+++ scripts/360dimBG.js
@@ -0,0 +1,20 @@
+var fg = document.getElementById("fgSection"),
+    mousemoveSince = new Date();
+
+fg.style.opacity = "1";
+
+fg.addEventListener('mousemove', ()=>{
+    mousemoveSince = new Date();
+})
+
+setInterval(()=>{
+    if(new Date() - mousemoveSince > 10000){
+        if (Number(fg.style.opacity) > 0){ 
+            fg.style.opacity = String(Number(fg.style.opacity)-0.05);
+        }
+    } else {
+        if (Number(fg.style.opacity) < 1){
+            fg.style.opacity = String(Number(fg.style.opacity)+0.15);
+        }
+    }
+}, 0);
